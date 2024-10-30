4c7a7a1d74dfe81f62f8199d4aedf0010c5f4d0f
jaqsyadam <157209550+jaqsyadam@users.noreply.github.com>
2024-10-30 16:43:28 +0500

    Create Todo.java

---
+++ task2/Todo.java
@@ -0,0 +1,24 @@
+public class Todo {
+    private int id;
+    private String title;
+    private boolean completed;
+
+    public Todo(int id, String title) {
+        this.id = id;
+        this.title = title;
+        this.completed = false;
+    }
+
+    // Getters and Setters
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setCompleted(boolean completed) {
+        this.completed = completed;
+    }
+}
